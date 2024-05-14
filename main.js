76af9d84f36e0cbcef08d462afc7cab4d079bd72
Gino M <85039742+Ginosprod@users.noreply.github.com>
2024-05-14 23:33:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // import * as BABYLON from 'babylonjs';
 // import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
 // import "@babylonjs/loaders";
-// import { AnimationEvent } from '@babylonjs/core';
+import { AnimationEvent } from '@babylonjs/core';
 // INTERFACE MENU
 const jeux = ["Jeu1","Jeu2","Jeu3","Jeu4"];
 const backinterface = document.getElementById("backinterface");
